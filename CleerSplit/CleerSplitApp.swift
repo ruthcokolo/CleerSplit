90f8c5f4c1f51a172d6a6533951bec0987a7819d
Ruth Okolo <ruthcokolo@outlook.com>
2025-12-29 16:50:51 -0500

    Initial Commit

---
+++ CleerSplit/CleerSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleerSplitApp.swift
+//  CleerSplit
+//
+//  Created by Chidinma on 2025-12-29.
+//
+
+import SwiftUI
+
+@main
+struct CleerSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
